21e0f1b457bf991044adebc2039eb5eb161f5b9e
0jafc0 <jafclive0@gmail.com>
2021-09-16 13:30:52 -0300

    corrigido algumas logicas na funcao gerarSenha e na verificarChecks, adicionado inicializador

--- codigo.js
+++ codigo.js
@@ -2,11 +2,11 @@
 // verifica os inputs checks e retorna uma lista com as opções escolhidas.
 function verificaChecks(){
 
-    let minusculas = document.forms[0].minusculas;
-    let maiusculas = document.forms[0].maiusculas;
-    let numeros = document.forms[0].numeros;
-    let especiais = document.forms[0].especiais;
-    const listas = [];
+    let minusculas = document.getElementById("minusculas");
+    let maiusculas = document.getElementById("maiusculas");
+    let numeros = document.getElementById("numeros");
+    let especiais = document.getElementById("especiais");
+    let listas = [];
 
     if(minusculas.checked === true){
         listas.push("abcdefghijklmnopqrstuvwxyz");
@@ -21,45 +21,40 @@ function verificaChecks(){
         listas.push("!@#$%*()_+=^~?");
     }
 
-    if(listas === null){
-        listas.push("abcdefghijklmnopqrstuvwxyz");
-    }
-    console.log(listas)
     return listas;
-}
+};
 
 // gera a senha com as opções escolhidas.
 function geraSenha(){
     //variaveis
-    let divAreaSenhas = document.getElementById("divAreaSenhas");
-    let campoDeSenha = "";
-    let areaDeSenhas;
-    let tamanho = parseInt(document.forms[0].campo2.value);
+    let campoDeSenha = document.getElementById("campoSenha");
+    let tamanho = parseInt(document.getElementById("inputTamanho").value);
     let listas = verificaChecks();
+    let senha = "";
 
-    campoDeSenha = "";
-    for(var i = 0;i < tamanho;i++){
+    for(let i = 0;i < tamanho;i++){
         let opcoes = Math.floor(Math.random() * listas.length);
-        campoDeSenha += listas[opcoes].charAt(Math.floor(Math.random()*listas[opcoes].length));
+        senha += listas[opcoes].charAt(Math.floor(Math.random()*listas[opcoes].length));
     }
-
-    //configurações dos atributos da textarea de senhas
-    areaDeSenhas = document.createElement("textarea");
-    areaDeSenhas.setAttribute("id","areaDeSenhas");
-    areaDeSenhas.setAttribute("cols",tamanho);
-    areaDeSenhas.setAttribute("rows","10");
-
-    if(document.getElementById("areaDeSenhas") !== null){
-        document.getElementById("areaDeSenhas").textContent += " " + campoDeSenha;
-    }else{
-        areaDeSenhas.textContent = campoDeSenha + " ";
-        divAreaSenhas.appendChild(areaDeSenhas);
-    }
-    document.getElementById("areaDeSenhas").textContent.replace(/\s/g,"\n");
-    
-}
+    campoDeSenha.value = senha;
+};
 
 //main
-document.addEventListener("submit", function(){
+window.addEventListener("submit", function(){
     geraSenha();
 });
+
+//inicializador
+window.onload = function(){
+    let inputRange = document.getElementById("rangeTamanho");
+    let inputTamanho = document.getElementById("inputTamanho");
+    inputRange.addEventListener("change", function(){
+        inputTamanho.value = inputRange.value;
+    });
+
+    inputTamanho.addEventListener("change", function(){
+        inputRange.value = inputTamanho.value;
+    });
+
+    geraSenha();
+};
